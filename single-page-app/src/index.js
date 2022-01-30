78c8378ebd928ee8102389c1ed99e9492e0fbc18
asananddevsingh <ananddevsingh.aiet@gmail.com>
2022-01-31 02:09:04 +0530

    Loader and Asset modules examples

--- single-page-app/src/index.js
+++ single-page-app/src/index.js
@@ -1,3 +1,11 @@
 import { HelloWorld } from './components/helloWorld/helloWorld';
+import { MyButton } from './components/myButton/myButton';
+import { MyImage } from './components/myImage/myImage';
 
 HelloWorld();
+
+const myImg = new MyImage();
+myImg.render();
+
+const myBtn = new MyButton();
+myBtn.render();
--- single-page-app/webpack.config.js
+++ single-page-app/webpack.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|jpeg)$/,
+        type: 'asset', // 'asset/resource', 'asset/inline', 'asset/source'.
+        parser: {
+          dataUrlCondition: {
+            maxSize: 5 * 1024, //5kb. // It's default size is 8kb. That means anything greater than this size will be not part of bundle.
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
